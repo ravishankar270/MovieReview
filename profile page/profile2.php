f23df2cc0a89b7aae4f98a84a4d57ed56c3e6e35
SherAli716 <shaikhsherali81z@gmail.com>
2020-11-12 17:11:32 +0530

    updated

--- profile page/profile2.php
+++ profile page/profile2.php
@@ -10,7 +10,27 @@
     <title>Profile Card</title>
 </head>
 <body>
- 
+    <?php
+
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'moviereview');
+    
+    // Try connecting to the Database
+    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        if($conn->connect_error){
+            echo "$conn->connect_error";
+            die("Connection Failed : ". $conn->connect_error);
+        } else {
+            
+        $sql="SELECT * from `user`";
+    
+        }
+        $result = mysqli_query( $conn,$sql);
+        $resultcheck = mysqli_num_rows($result);
+        
+?>
     <!-- <h3 style="color:aliceblue; text-align: center; padding-top:50px">Profile</h3> -->
     <div class="modal">
         <img src="img/pic3.jpeg" alt="">
@@ -26,13 +46,18 @@
                 <a href="#" class="mail">
                     <i class="far fa-envelope"></i>
                 </a>
+
+                <?php
+    while ($row = mysqli_fetch_assoc($result))
+        
+?>
                 <div class="main">
                     <div class="image">
                         <div class="hover">
                             <i class="fas fa-camera fa-2x"></i>
                         </div>
                     </div>
-                    <h3 class="name">John Wright</h3>
+                    <h3 class="name"><?php echo $row['Name']; ?></h3>
                     <h3 class="sub-name">@J_wright</h3>
                 </div>
             </div>
@@ -84,6 +109,7 @@
             </div>
         </div>
     </div>
+
     <script src="app.js"></script>
 </body>
 </html>
