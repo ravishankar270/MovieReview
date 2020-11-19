276803afdc772cfd27a1f7a9b0a72f4fe14c4a6d
SherAli716 <shaikhsherali81z@gmail.com>
2020-11-19 11:53:26 +0530

    updated

--- registration/register.php
+++ registration/register.php
@@ -17,7 +17,7 @@ $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
         $sql="INSERT INTO `user` (`user_id`, `username`, `password`, `Phone_no`, `Email_id`, `cliend_id`) VALUES (NULL, '$_POST[username]', '$_POST[password]', '', '', '')";
     }
 
-    echo "$_POST[password]";
+    
     $run = mysqli_query( $conn,$sql);
 	if($run == TRUE) {
         echo "Updated";
