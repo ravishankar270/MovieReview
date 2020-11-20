9e716bec658df42014889c4b72485118bb9e7fc1
SherAli716 <shaikhsherali81z@gmail.com>
2020-11-20 11:22:29 +0530

    quiz page done

--- quiz/question.js
+++ quiz/question.js
@@ -1,15 +1,13 @@
-src="question.js"
-
 const quiz = [
     {
         q: 'Which month comes right after june',
         options:['may','sep','july','august'],
-        answer:0
+        answer:2
     },
     {
         q: 'Which month comes right after july',
         options:['may','sep','july','august'],
-        answer:0
+        answer:1
     },
     {
         q: '3 + 4 = 7 ?',
