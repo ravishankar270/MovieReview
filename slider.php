ffa2bec8bece440c27f2739227e432bf3379d3cb
Ravi <drogon1232019@gmail.com>
2020-11-21 13:25:23 +0530

    fixed tabpanel

--- slider.php
+++ slider.php
@@ -1,5 +1,98 @@
+<style type="text/css">
+    .slider
+    {
+        height: 600px;
+        width: 100%;
+        overflow: hidden;
+        
+    }
+    .sliderchild{
+        height: 600px;
+        width:500%;
+        animation: slider infinite 20s 2s;
+    }
+    .imagecon{
+        height:600px;
+        width: 20%;
+        background-color: gray;
+        float: left;
+        line-height: 500px;
+        color:white;
+        text-align: left;
+        font-size: 70px;
+        font-family: 'Times New Roman', Times, serif;
 
-<link rel="stylesheet" type="text/css" href="../slider.css?v=<?php echo time(); ?>">
+    }
+    
+    .sliderchild a{
+        padding-left: 50px;
+        padding-top: 100px;
+        color: white;
+    }
+
+
+    .imagecon:nth-child(1){
+        background-image: url('..homepage/homepage-images/joker.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    
+    .imagecon:nth-child(2){
+        background-image: url('..homepage/homepage-images/parasite.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .imagecon:nth-child(3){
+        background-image: url('../homepage/homepage-images/007.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .imagecon:nth-child(4){
+        background-image: url('../homepage/homepage-images/widow.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+     .imagecon:nth-child(5){
+        background-image: url('../homepage/homepage-images/joker.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    } 
+
+     @keyframes slider{
+        12.5%
+        {
+            margin-left: -100%;
+        }
+        25%
+        {
+            margin-left: -100%;
+        }
+        37.5%
+        {
+            margin-left: -200%;
+        }
+        50%
+        {
+            margin-left: -200%;
+        }
+        62.5%
+        {
+            margin-left: -300%;
+        }
+        75%
+        {
+            margin-left: -300%;
+        }
+        87.5%
+        {
+            margin-left: -400%;
+        }
+        100%
+        {
+            margin-left: -400%;
+        }
+    }
+</style>
 <div class="slider">
             <div class="sliderchild">
                 <div class="imagecon">
