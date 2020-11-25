362ead2e5510318615812792a13744a783d86125
SherAli716 <shaikhsherali81z@gmail.com>
2020-11-25 10:38:21 +0530

    commit

--- registration/complete.php
+++ registration/complete.php
@@ -1,7 +1,9 @@
 <?php
-  echo "You have successfully Registered ";
-  echo "Go back to Login";
+  
+//   echo "Go back to Login";
 ?>
+<h2>You have successfully Registered</h2>
+<h3>Please Go back to Login</h3>
 
 <form action="../login/login.php">
     <input type="submit" value="Go to Login" />
