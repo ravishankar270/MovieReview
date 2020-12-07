a50a73337afb07f8b133f36bf29977466f5c3622
Ravi <drogon1232019@gmail.com>
2020-12-07 11:33:36 +0530

    connection

--- login/login.php
+++ login/login.php
@@ -1,3 +1,5 @@
+<?php session_start(); 
+?>
 <!DOCTYPE html>
 
 
@@ -17,7 +19,16 @@
 
 <?php
 
-    include('google.php');
+require_once('googleapis/vendor/autoload.php');
+
+    $google_client=new Google_client();
+    $google_client->setClientId("16817687725-0vn2hhn7m3pdfuk7mcukjmgutb5mj88s.apps.googleusercontent.com");
+    $google_client->setClientSecret('fmkbIn_UKTwgKKIn8uH-R3CX');
+    $google_client->setRedirectUri('http://localhost/login/login/home.php');
+    
+    $google_client->addScope('email');
+    $google_client->addScope('profile');
+    
     
     if(isset($_SESSION['id'])){
         header('Location: index.php');
