3257501f8f1c0aa561efc927dca095ca935c1e85
Ravi <drogon1232019@gmail.com>
2020-12-12 01:23:59 +0530

    hello

--- connectdb.php
+++ connectdb.php
@@ -1,19 +1,19 @@
 <?php
-        //development
+  //       development
 		// $servername = "localhost";
   //       $username = "id15627207_ravi";
   //       $password = "R8652705916r@";
   //       $dbname="id15627207_moviereview";
         
         // remote sql
-        // $servername = "remotemysql.com";
-        // $username = "buU5l6jIDM";
-        // $password = "v2BL9f8Kwy";
-        // $dbname="buU5l6jIDM";
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname="moviereview";
+        $servername = "remotemysql.com";
+        $username = "buU5l6jIDM";
+        $password = "v2BL9f8Kwy";
+        $dbname="buU5l6jIDM";
+        // $servername = "localhost";
+        // $username = "root";
+        // $password = "";
+        // $dbname="moviereview";
     
     // Create connection
     $conn = new mysqli($servername, $username, $password,$dbname);
